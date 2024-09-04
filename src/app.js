0e5a4d56034a7995e96d6e76d06595d7ebe7130c
jeet1desai <jeetdesai995@gmail.com>
2024-09-04 21:19:59 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import logger from 'morgan';
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import indexRouter from './routes/index';
 
+import { connectDB } from './config/db';
+
+connectDB();
+
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,1 +1,4 @@
 export * from './home';
+export * from './auth';
+export * from './event';
+export * from './dashboard';
